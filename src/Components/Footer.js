a4a7da328df8beb5757373a1831d7b6af02e0c24
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-04 20:37:34 -0400

    footer added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Footer(props){
-    return <h1>Footer</h1>
-};
+const Footer = () => (
+<footer className="footer footer-center p-4 bg-base-300 text-base-content">
+  <div>
+    <p>Copyright © 2022 - All right reserved by TNBC Ltd</p>
+  </div>
+</footer>
+);
 
 export default Footer;
+
